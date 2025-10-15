47c2f2999413767699ea65cdf24318116115c097
casComputer <229486028+casComputer@users.noreply.github.com>
2025-10-15 19:22:19 +0530

    signup: get all form data

--- client/src/store/app.store.ts
+++ client/src/store/app.store.ts
@@ -1,8 +1,9 @@
+
 import { create } from "zustand";
 
-export const useAppStore = create((set) => ({
-  user: {
-    fullName: "",
-    username: "",
-  },
+export const useAppStore = create(set => ({
+    user: {
+        role: "student"
+    }, 
+    setUser: user => set({ user })
 }));

3da4f6671f3d28aa9e3422f693e8379c608e7701
casDepartment <229486028+casComputer@users.noreply.github.com>
2025-10-17 13:47:54 +0530

    connnected to server

--- client/src/controller/auth/signup.controller.js
+++ client/src/controller/auth/signup.controller.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 
-const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://localhost:3000";
+const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://192.168.20.90:3000";
+
+console.log("API_URL:", API_URL);
 
 const signup = async (data) => {
-    try {
-        const response = await axios.post(`${API_URL}/auth/signup`, {
-            ...data
-        });
-        return response.data;
-    } catch (error) {
-        console.error("Error signing up:", error);
-        throw error;
-    }
+  try {
+    const response = await axios.post(`${API_URL}/auth/signup`, {
+      ...data,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error signing up:", error);
+    throw error;
+  }
 };
 
 export default signup;

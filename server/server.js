fd06c459695a9e25b54d18f9ed825052e5a69915
casComputer <229486028+casComputer@users.noreply.github.com>
2025-10-19 11:16:12 +0530

    ...

--- server/server.js
+++ server/server.js
@@ -1,21 +1,7 @@
-import express from "express";
-import cors from "cors";
-
-import authRoutes from "./routes/auth.routes.js";
+import express from 'express';
 
 const app = express();
 const PORT = 3000;
-
-app.use(express.json());
-
-const corsOptions = {
-  origin: ["http://192.168.20.90:3000", "*"], // Allowed origins
-  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-};
-
-app.use(cors(corsOptions));
-app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);

a813ca6d5e1c4f1905c6b0989570a565325189ef
casComputer <229486028+casComputer@users.noreply.github.com>
2025-10-21 21:21:36 +0530

    code improvability and interceptor implemented

--- server/controllers/auth/signin.controller.js
+++ server/controllers/auth/signin.controller.js
@@ -1,46 +1,43 @@
+import "dotenv/config";
 import { turso } from "../../config/turso.js";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import { comparePassword, generateTokens } from "./authUtils.js";
 
-const sigininController = async(req, res) => {
-  const { username, password } = req.body;
+const signinController = async (req, res) => {
+    try {
+        const { username, password } = req.body;
 
-    const user = await turso.execute(
-        `SELECT * FROM students WHERE username = ?`,
-        [username]
-    )
+        const userResult = await turso.execute(
+            `SELECT * FROM students WHERE username = ?`,
+            [username]
+        );
+        if (!userResult.rows.length) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    error: "Invalid username or password"
+                });
+        }
 
-    if (!user.rows.length) {
-        return res
-        .status(400)
-        .json({ success: false, error: "Invalid username or password" });
+        const user = userResult.rows[0];
+
+        const validPassword = await comparePassword(password, user.password);
+        if (!validPassword) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    error: "Invalid username or password"
+                });
+        }
+
+        const tokens = generateTokens(username, user.role);
+
+        res.json({ success: true, ...tokens, data: user });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false, error: "Server error" });
     }
+};
 
-
-    const validPassword = bcrypt.compareSync(password, user.rows[0].password);
-
-    if (!validPassword) {
-        return res
-        .status(400)
-        .json({ success: false, error: "Invalid username or password" });
-    }
-
-    const payload = { username, role: user.rows[0].role };
-    let options = { expiresIn: "1m" }
-
-    const accessToken = jwt.sign(
-        payload,
-        process.env.JWT_ACCESS_TOKEN_SECRET,
-        options
-    );
-
-    const refreshToken = jwt.sign(
-        payload,
-        process.env.JWT_REFRESH_TOKEN_SECRET,
-        { expiresIn: "7d" }
-    );
-
-    res.json({ success: true, accessToken, refreshToken, data: user.rows[0] });
-}  
-
-export default sigininController;
+export default signinController;

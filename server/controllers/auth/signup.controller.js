1d21c7148a73958b77fe1351eeec60d37a5889fe
casComputer <229486028+casComputer@users.noreply.github.com>
2025-10-22 12:06:13 +0530

    fix auth

--- server/controllers/auth/signup.controller.js
+++ server/controllers/auth/signup.controller.js
@@ -1,53 +1,72 @@
 import "dotenv/config";
 import { turso } from "../../config/turso.js";
 import {
-    hashPassword,
-    generateTokens,
-    validateSignupFields
-} from "./authUtils.js";
+  hashPassword,
+  generateTokens,
+  validateSignupFields,
+} from "../../utils/auth.utils.js";
 
 const signupController = async (req, res) => {
-    try {
-        const { username, password, fullName, userRole, course, year } =
-            req.body;
+  try {
+    const { username, password, fullName, userRole, course, year } = req.body;
+    console.log(username);
 
-        validateSignupFields(req.body);
+    validateSignupFields(req.body);
 
-        // Check if username already exists
-        const existUser = await turso.execute(
-            `SELECT * FROM students WHERE username = ?`,
-            [username]
-        );
-        if (existUser.rows.length) {
-            return res
-                .status(400)
-                .json({ success: false, error: "Username already exists" });
-        }
+    console.log("validation success");
 
-        const hashedPassword = await hashPassword(password);
+    // Check if username already exists
+    const existUser = await turso.execute(
+      `
+        SELECT studentId as id FROM students WHERE studentId = ?
+        UNION
+        SELECT teacherId as id FROM teachers WHERE teacherId = ?
+        UNION
+        SELECT parentId as id FROM parents WHERE parentId = ?
+        UNION
+        SELECT adminId as id FROM admin WHERE adminId = ?
+        `,
+      [username, username, username, username]
+    );
 
-        if (userRole === "student") {
-            await turso.execute(
-                `INSERT INTO students (username, fullname, password, role, course, year_of_study) VALUES (?, ?, ?, ?, ?, ?)`,
-                [username, fullName, hashedPassword, userRole, course, year]
-            );
-        } else {
-            await turso.execute(
-                `INSERT INTO students (username, fullname, password, role) VALUES (?, ?, ?, ?)`,
-                [username, fullName, hashedPassword, userRole]
-            );
-        }
-
-        const tokens = generateTokens(username, userRole);
-
-        res.json({ success: true, ...tokens, data: req.body });
-    } catch (err) {
-        console.error(err);
-        res.status(400).json({
-            success: false,
-            error: err.message || "Server error"
-        });
+    if (existUser.rows.length > 0) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Username already exists" });
     }
+
+    const hashedPassword = await hashPassword(password);
+
+    if (userRole === "student") {
+      await turso.execute(
+        `INSERT INTO students (studentId, fullname, password, course, year_of_study) VALUES (?, ?, ?, ?, ?)`,
+        [username, fullName, hashedPassword, course, year]
+      );
+      console.log("student added");
+    } else if (userRole === "parent") {
+      await turso.execute(
+        `INSERT INTO parents (parentId, fullname, password) VALUES (?, ?, ?)`,
+        [username, fullName, hashedPassword]
+      );
+      console.log("parent added");
+    } else if (userRole === "teacher") {
+      await turso.execute(
+        `INSERT INTO teachers (teacherId, fullname, password) VALUES (?, ?, ?)`,
+        [username, fullName, hashedPassword]
+      );
+      console.log("teacher added");
+    }
+
+    const tokens = generateTokens(username, userRole);
+
+    res.json({ success: true, ...tokens, data: req.body });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({
+      success: false,
+      error: err.message || "Server error",
+    });
+  }
 };
 
 export default signupController;

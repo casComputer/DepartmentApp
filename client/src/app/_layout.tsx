dc36c781d1cc99e743cbe55ff4afe537a5293884
casDepartment <229486028+casComputer@users.noreply.github.com>
2025-10-22 16:25:22 +0530

    store user in mmkv and tokens in secure storage

--- client/src/app/_layout.tsx
+++ client/src/app/_layout.tsx
@@ -1,29 +1,35 @@
+import "../../global.css";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { View, useColorScheme } from "react-native";
-import "../../global.css";
+
+import {getUser} from "@/src/utils/storage"
 
 export default function RootLayout() {
-    const theme = useColorScheme();
+  const theme = useColorScheme();
+ const user = getUser()
+  
 
-    return (
-        <View className="${theme === 'dark' ? 'dark': ''} flex-1 ${theme== 'dark' ? 'bg-black' : 'bg-white' }">
-            <StatusBar style="auto" animated />
+  return (
+    <View className="${theme === 'dark' ? 'dark': ''} flex-1 ${theme== 'dark' ? 'bg-black' : 'bg-white' }">
+      <StatusBar style="auto" animated />
 
-            <Stack screenOptions={{ headerShown: false }}>
-                <Stack.Screen
-                    name="index"
-                    options={{ animation: "slide_from_right" }}
-                />
-                <Stack.Screen
-                    name="auth/Signin"
-                    options={{ animation: "slide_from_right" }}
-                />
-                <Stack.Screen
-                    name="auth/Signup"
-                    options={{ animation: "slide_from_right" }}
-                />
-            </Stack>
-        </View>
-    );
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Protected guard={true}>
+          <Stack.Screen
+            name="index"
+            options={{ animation: "slide_from_right" }}
+          />
+          <Stack.Screen
+            name="auth/Signin"
+            options={{ animation: "slide_from_right" }}
+          />
+          <Stack.Screen
+            name="auth/Signup"
+            options={{ animation: "slide_from_right" }}
+          />
+        </Stack.Protected>
+      </Stack>
+    </View>
+  );
 }
--- client/src/utils/storage.ts
+++ client/src/utils/storage.ts
@@ -3,31 +3,39 @@ import { createMMKV } from "react-native-mmkv";
 export const storage = createMMKV();
 
 type UserData = {
-  username: string;
+  userId: string;
   fullname: string;
   role: string;
   course: string;
-  year: string;
+  year_of_study: string;
 };
 
-export const setUserData = ({
-  username,
+export const setUser = ({
+  userId,
   fullname,
   role,
-  course,
-  year,
+  course = "",
+  year_of_study = "",
 }: UserData) => {
-  storage.set("username", username);
+  storage.set("userId", userId);
   storage.set("fullname", fullname);
   storage.set("role", role);
   storage.set("course", course);
-  storage.set("year", year);
+  storage.set("year", year_of_study);
 };
 
-export const clearUser = () => {
-  storage.remove("username");
-  storage.remove("fullname");
-  storage.remove("role");
-  storage.remove("course");
-  storage.remove("year");
+export const getUser = () => {
+  const userId = storage.getString("userId");
+  const fullname = storage.getString("fullname");
+  const role = storage.getString("role");
+  const course = storage.getString("course");
+  const year_of_study = storage.getString("year_of_study");
+
+  return {
+    userId,
+    fullname,
+    role,
+    course,
+    year_of_study,
+  };
 };

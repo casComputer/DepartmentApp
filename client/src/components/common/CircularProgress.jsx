17089725a7f6c6e13c58ff866a414a378a877bd5
CasComputer <229486028+casComputer@users.noreply.github.com>
2025-11-11 15:22:12 +0530

    ...

--- client/src/components/common/CircularProgress.jsx
+++ client/src/components/common/CircularProgress.jsx
@@ -1,73 +1,74 @@
-// CircularProgress.js
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Svg, { Circle } from "react-native-svg";
 
 const CircularProgress = ({
-  size = 100,
-  strokeWidth = 10,
-  progress = 83,
-  maxProgress = 100,
-  showPercentage = true,
+    size = 100,
+    strokeWidth = 10,
+    progress = 83,
+    maxProgress = 100,
+    showPercentage = true,
+    strokeFillColor = "#4F46E5", 
+
 }) => {
-  const radius = (size - strokeWidth) / 2;
-  const circumference = 2 * Math.PI * radius;
-  const normalizedProgress = Math.min(progress, maxProgress);
-  const strokeDashoffset =
-    circumference - (normalizedProgress / maxProgress) * circumference;
+    const radius = (size - strokeWidth) / 2;
+    const circumference = 2 * Math.PI * radius;
+    const normalizedProgress = Math.min(progress, maxProgress);
+    const strokeDashoffset =
+        circumference - (normalizedProgress / maxProgress) * circumference;
 
-  return (
-    <View className="justify-center items-center ">
-      <Svg width={size} height={size}>
-        {/* Background circle */}
-        <Circle
-          stroke="#e6e6e6"
-          fill="none"
-          cx={size / 2}
-          cy={size / 2}
-          r={radius}
-          strokeWidth={strokeWidth}
-        />
-        {/* Progress circle */}
-        <Circle
-          stroke="#4F46E5"
-          fill="none"
-          cx={size / 2}
-          cy={size / 2}
-          r={radius}
-          strokeWidth={strokeWidth}
-          strokeDasharray={circumference}
-          strokeDashoffset={strokeDashoffset}
-          strokeLinecap="round"
-          rotation="-90"
-          originX={size / 2}
-          originY={size / 2}
-        />
-      </Svg>
+    return (
+        <View className="justify-center items-center ">
+            <Svg width={size} height={size}>
+                {/* Background circle */}
+                <Circle
+                    stroke={'#e6e6e6'}
+                    fill="none"
+                    cx={size / 2}
+                    cy={size / 2}
+                    r={radius}
+                    strokeWidth={strokeWidth}
+                />
+                {/* Progress circle */}
+                <Circle
+                    stroke={strokeFillColor}
+                    fill="none"
+                    cx={size / 2}
+                    cy={size / 2}
+                    r={radius}
+                    strokeWidth={strokeWidth}
+                    strokeDasharray={circumference}
+                    strokeDashoffset={strokeDashoffset}
+                    strokeLinecap="round"
+                    rotation="-90"
+                    originX={size / 2}
+                    originY={size / 2}
+                />
+            </Svg>
 
-      <View style={styles.textContainer}>
-        <Text style={styles.text}>
-          {normalizedProgress}
-          {showPercentage && "%"}
-        </Text>
-      </View>
-    </View>
-  );
+            <View style={styles.textContainer}>
+                <Text style={styles.text}>
+                    {normalizedProgress}
+                    {showPercentage && "%"}
+                </Text>
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  textContainer: {
-    position: "absolute",
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: "600",
-    color: "#4F46E5",
-  },
+    container: {
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    textContainer: {
+        position: "absolute"
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: "600",
+        color: "#4F46E5"
+    }
 });
 
 export default CircularProgress;
--- client/src/components/student/home/MonthlyAttendenceMiniReport.tsx
+++ client/src/components/student/home/MonthlyAttendenceMiniReport.tsx
@@ -4,35 +4,51 @@ import React from "react";
 import CircularProgress from "@components/common/CircularProgress";
 
 export default function MonthlyAttendenceMiniReport() {
-  return (
-    <TouchableOpacity className="shadow bg-white w-[90%] mx-auto mt-12  rounded-3xl overflow-hidden p-5 gap-8 flex-row justify-between items-center">
-        <View className="flex-1">
-          <CircularProgress progress={83} />
-          <Text adjustsFontSizeToFit numberOfLines={1} className="text-center text-lg font-semibold mt-4">
-            Attendance
-          </Text>
-        </View>
-        <View className="flex-1">
-          <CircularProgress
-            progress={3}
-            maxProgress={30}
-            showPercentage={false}
-          />
-          <Text adjustsFontSizeToFit numberOfLines={1} className="text-center text-lg font-semibold mt-4">
-            Leave Taken
-          </Text>
-        </View>
-        <View className="flex-1">
-          <CircularProgress
-            progress={23}
-            showPercentage={false}
-            maxProgress={30}
-          />
-          <Text adjustsFontSizeToFit numberOfLines={1} className="text-center text-lg font-semibold mt-4">
-            Ongoing Days
-          </Text>
-        </View>
-      
-    </TouchableOpacity>
-  );
+    return (
+        <TouchableOpacity className="shadow bg-white w-[90%] mx-auto mt-12  rounded-3xl overflow-hidden p-5 gap-8 flex-row justify-between items-center">
+            <View className="flex-1">
+                <CircularProgress
+                    progress={83}
+                    strokeFillColor={"rgb(247,55,159)"}
+                />
+                <Text
+                    adjustsFontSizeToFit
+                    numberOfLines={1}
+                    className="text-center text-lg font-semibold mt-4"
+                >
+                    Attendance
+                </Text>
+            </View>
+            <View className="flex-1">
+                <CircularProgress
+                    progress={3}
+                    maxProgress={30}
+                    showPercentage={false}
+                    strokeFillColor={"rgb(247,55,159)"}
+                />
+                <Text
+                    adjustsFontSizeToFit
+                    numberOfLines={1}
+                    className="text-center text-lg font-semibold mt-4"
+                >
+                    Leave Taken
+                </Text>
+            </View>
+            <View className="flex-1">
+                <CircularProgress
+                    progress={23}
+                    showPercentage={false}
+                    maxProgress={30}
+                    strokeFillColor={"rgb(247,55,159)"}
+                />
+                <Text
+                    adjustsFontSizeToFit
+                    numberOfLines={1}
+                    className="text-center text-lg font-semibold mt-4"
+                >
+                    Ongoing Days
+                </Text>
+            </View>
+        </TouchableOpacity>
+    );
 }

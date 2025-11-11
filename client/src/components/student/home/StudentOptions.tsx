2a5f0cbbc62979c104cb57a19bf508d439ab9d8d
CasComputer <229486028+casComputer@users.noreply.github.com>
2025-11-11 15:22:12 +0530

    ...

--- client/src/components/student/home/StudentOptions.tsx
+++ client/src/components/student/home/StudentOptions.tsx
@@ -1,98 +1,164 @@
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import {
-  AntDesign,
-  Entypo,
-  Feather,
-  FontAwesome,
-  FontAwesome6,
-  MaterialCommunityIcons,
+    AntDesign,
+    Entypo,
+    Feather,
+    FontAwesome,
+    FontAwesome6,
+    MaterialCommunityIcons,
+    SimpleLineIcons
 } from "@expo/vector-icons";
 
 const ICONS_SIZE = 40;
 
 const StudentOptions = () => {
-  return (
-    <View className="px-6 mt-12 flex-1">
-      <View className="shadow bg-white w-full rounded-3xl gap-3 overflow-hidden py-3">
-        {/* First Row of Options */}
-        <View className="flex-row justify-between">
-          <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1 ">
-            <MaterialCommunityIcons
-              name="chat-question"
-              size={ICONS_SIZE}
-              color="black"
-            />
+    return (
+        <View className="px-6 mt-12 flex-1">
+            <View className="shadow bg-white w-full rounded-3xl gap-3 overflow-hidden py-3">
+                {/* First Row of Options */}
+                <View className="flex-row justify-between">
+                    <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1 ">
+                        <MaterialCommunityIcons
+                            name="chat-question"
+                            size={ICONS_SIZE}
+                            color="black"
+                        />
 
-            <Text adjustsFontSizeToFit numberOfLines={1} className="text-lg font-semibold">Ask Leave</Text>
-          </TouchableOpacity>
+                        <Text
+                            adjustsFontSizeToFit
+                            numberOfLines={1}
+                            className="text-lg font-semibold"
+                        >
+                            Ask Leave
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1 ">
+                        <SimpleLineIcons
+                            name="note"
+                            size={ICONS_SIZE}
+                            color="black"
+                        />
 
-          <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
-            <Image
-              style={{ width: ICONS_SIZE, height: ICONS_SIZE }}
-              source={require("@assets/images/icons/attendence.png")}
-            />
+                        <Text
+                            adjustsFontSizeToFit
+                            numberOfLines={1}
+                            className="text-lg font-semibold"
+                        >
+                            Attendence
+                        </Text>
+                    </TouchableOpacity>
 
-            <Text adjustsFontSizeToFit numberOfLines={1} className="text-lg font-semibold">Attendence</Text>
-          </TouchableOpacity>
+                    <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1 ">
+                        <SimpleLineIcons
+                            name="notebook"
+                            size={ICONS_SIZE}
+                            color="black"
+                        />
 
-          <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
-            <Image
-              style={{ width: ICONS_SIZE, height: ICONS_SIZE }}
-              source={require("@assets/images/icons/assignment.png")}
-            />
+                        <Text
+                            adjustsFontSizeToFit
+                            numberOfLines={1}
+                            className="text-lg font-semibold"
+                        >
+                            Assignment
+                        </Text>
+                    </TouchableOpacity>
+                </View>
 
-            <Text adjustsFontSizeToFit numberOfLines={1} className="text-lg font-semibold">Assignment</Text>
-          </TouchableOpacity>
+                {/* Second Row of Options */}
+
+                <View className="flex-row justify-between">
+                    <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
+                        <FontAwesome6
+                            name="hand-holding-dollar"
+                            size={ICONS_SIZE}
+                            color="black"
+                        />
+
+                        <Text
+                            adjustsFontSizeToFit
+                            numberOfLines={1}
+                            className="text-lg font-semibold"
+                        >
+                            Fees
+                        </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
+                        <AntDesign
+                            name="file-search"
+                            size={ICONS_SIZE}
+                            color="black"
+                        />
+
+                        <Text
+                            adjustsFontSizeToFit
+                            numberOfLines={1}
+                            className="text-lg font-semibold"
+                        >
+                            Exam Results
+                        </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
+                        <AntDesign
+                            name="message"
+                            size={ICONS_SIZE}
+                            color="black"
+                        />
+
+                        <Text className="text-lg font-semibold">Chat</Text>
+                    </TouchableOpacity>
+                </View>
+
+                {/* Third Row of Options */}
+                <View className="flex-row justify-between">
+                    <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
+                        <Entypo name="book" size={ICONS_SIZE} color="black" />
+                        <Text
+                            adjustsFontSizeToFit
+                            numberOfLines={1}
+                            className="text-lg font-semibold"
+                        >
+                            Notes
+                        </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
+                        <Feather
+                            name="check-circle"
+                            size={ICONS_SIZE}
+                            color="black"
+                        />
+
+                        <Text
+                            adjustsFontSizeToFit
+                            numberOfLines={1}
+                            className="text-lg font-semibold"
+                        >
+                            Internal Marks
+                        </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
+                        <FontAwesome
+                            name="graduation-cap"
+                            size={ICONS_SIZE}
+                            color="black"
+                        />
+
+                        <Text
+                            adjustsFontSizeToFit
+                            numberOfLines={1}
+                            className="text-lg font-semibold"
+                        >
+                            Course
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
-
-        {/* Second Row of Options */}
-
-        <View className="flex-row justify-between">
-          <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
-            <FontAwesome6
-              name="hand-holding-dollar"
-              size={ICONS_SIZE}
-              color="black"
-            />
-
-            <Text adjustsFontSizeToFit numberOfLines={1} className="text-lg font-semibold">Fees</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
-            <AntDesign name="file-search" size={ICONS_SIZE} color="black" />
-
-            <Text adjustsFontSizeToFit numberOfLines={1} className="text-lg font-semibold">Exam Results</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
-            <AntDesign name="message" size={ICONS_SIZE} color="black" />
-
-            <Text className="text-lg font-semibold">Chat</Text>
-          </TouchableOpacity>
-        </View>
-
-        {/* Third Row of Options */}
-        <View className="flex-row justify-between">
-          <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
-          <Entypo name="book" size={ICONS_SIZE} color="black" />
-            <Text adjustsFontSizeToFit numberOfLines={1} className="text-lg font-semibold">Notes</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
-          <Feather name="check-circle" size={ICONS_SIZE} color="black" />
-
-            <Text adjustsFontSizeToFit numberOfLines={1} className="text-lg font-semibold">Internal Marks</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity className="p-4 justify-center items-center gap-2 flex-1">
-          <FontAwesome name="graduation-cap" size={ICONS_SIZE} color="black" />
-
-            <Text adjustsFontSizeToFit numberOfLines={1} className="text-lg font-semibold">Course</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>
-  );
+    );
 };
 
 export default StudentOptions;

0d3f83ba5bce319362fc33fe0f585e1d8d706f6c
casDepartment <229486028+casComputer@users.noreply.github.com>
2025-11-13 15:44:01 +0530

    local

--- client/src/components/admin/TeacherItem.jsx
+++ client/src/components/admin/TeacherItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const TeacherItem = ({ fullname }) => {
   return (
-   <TouchableOpacity className="bg-white rounded-3xl px-4 py-7 my-2">
+   <TouchableOpacity className="bg-white rounded-3xl px-5 py-7 my-2">
        <Text className="text-2xl font-bold ">{fullname}</Text>
      </TouchableOpacity>
   )

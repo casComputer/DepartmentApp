364b03319c42cb38e19505495faea47102adb3e2
CasComputer <229486028+casComputer@users.noreply.github.com>
2025-11-20 17:32:06 +0530

    local before pull

--- client/src/app/(admin)/(others)/AssignClass.jsx
+++ client/src/app/(admin)/(others)/AssignClass.jsx
@@ -1,46 +1,68 @@
+import { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
 
-const data = [
-  { label: "Item 1", value: "1" },
-  { label: "Item 2", value: "2" },
-  { label: "Item 3", value: "3" },
-  { label: "Item 4", value: "4" },
-  { label: "Item 5", value: "5" },
-  { label: "Item 6", value: "6" },
-  { label: "Item 7", value: "7" },
-  { label: "Item 8", value: "8" },
-];
+import { COURSE, YEAR } from "../../../constants/yearAndCourse.js";
+
+const Selector = ({ title, options, setSelected, selected }) => (
+    <View className="border border-gray-500 mt-5 rounded-3xl py-8 px-2">
+        <Text className="pl-3 text-gray-900 text-[6vw] font-bold text-[6vw] mb-3">
+            Select the {title}:
+        </Text>
+        {
+            //
+            options.map(item => (
+                <TouchableOpacity
+                    onPress={() => setSelected(item.id)}
+                    key={item.id}
+                    className={`rounded-full px-5 py-6 ${
+                        selected === item.id ? "bg-violet-200" : ""
+                    } `}>
+                    <Text className="capitalize text-gray-900 text-2xl font-bold">
+                        {item.title}
+                    </Text>
+                </TouchableOpacity>
+            ))
+        }
+    </View>
+);
 
 const AssignClass = () => {
-  return (
-    <View className="flex-1 mt-12">
-      <View className="flex-row items-center gap-3 px-3">
-        <TouchableOpacity onPress={() => router.back()}>
-          <MaterialIcons
-            name="arrow-back-ios-new"
-            size={24}
-            color="black"
-          />
-        </TouchableOpacity>
-        <Text className="font-black text-[7vw]">Assign Class</Text>
-      </View>
+    const [selectedCourse, setSelectedCourse] = useState();
+    const [selectedYear, setSelectedYear] = useState();
 
+    return (
+        <View className="flex-1 mt-12 px-3">
+            <View className="flex-row items-center gap-3 mb-10">
+                <TouchableOpacity onPress={() => router.back()}>
+                    <MaterialIcons
+                        name="arrow-back-ios-new"
+                        size={24}
+                        color="black"
+                    />
+                </TouchableOpacity>
+                <Text className="font-black text-[7vw]">Assign Class</Text>
+            </View>
 
-    <Text className=" text-gray-900 text-xl mt-16 px-5 font-bold text-[6vw] ">
-      Select the class:
-      </Text>
+            <Selector
+                title={"Class"}
+                options={COURSE}
+                setSelected={setSelectedCourse}
+                selected={selectedCourse}
+            />
+            <Selector
+                title={"Year"}
+                options={YEAR}
+                setSelected={setSelectedYear}
+                selected={selectedYear}
+            />
 
-      <TouchableOpacity>
-        <View className="mx-5 mt-5 p-4 border border-gray-300 rounded-lg">
-          <Text className="text-gray-900 text-lg">BCA</Text>
+            <TouchableOpacity className="mt-8 w-full py-5 bg-green-500 rounded-3xl justify-center items-center">
+                <Text className="font-black text-[6vw] ">Assign</Text>
+            </TouchableOpacity>
         </View>
-      </TouchableOpacity>
-
-
-    </View>
-  );
+    );
 };
 
 export default AssignClass;

b1df9a4961f106e08f1318e9f4ef8224c76fd8ca
CasComputer <229486028+casComputer@users.noreply.github.com>
2025-11-20 17:32:06 +0530

    local before pull

---
+++ client/src/constants/yearAndCourse.js
@@ -0,0 +1,29 @@
+export const COURSE = [
+    {
+        title: "BCA",
+        id: "BCA"
+    },
+    {
+        title: "BSC",
+        id: "BSC"
+    }
+];
+
+export const YEAR = [
+    {
+        title: "first year",
+        id: "FIRST_YEAR"
+    },
+    {
+        title: "second year",
+        id: "SECOND_YEAR"
+    },
+    {
+        title: "third year",
+        id: "THIRD_YEAR"
+    },
+    {
+        title: "fourth year",
+        id: "FOURTH_YEAR"
+    }
+];

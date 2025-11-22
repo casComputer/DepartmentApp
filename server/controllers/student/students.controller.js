853d468dc06240915b27fba647a99b7261e1281d
casComputer <229486028+casComputer@users.noreply.github.com>
2025-11-22 22:38:49 +0530

    fix typo

--- server/controllers/student/students.controller.js
+++ server/controllers/student/students.controller.js
@@ -87,3 +87,59 @@ export const fetchStudentsByClassTeacher = async (req, res) => {
         });
     }
 }
+
+export const verifyStudent = async (req, res) => {
+    const { studentId } = req.body;
+   
+    try {
+        await turso.execute(
+           'UPDATE students SET is_verified = true WHERE studentId = ?',
+           [studentId]
+        );
+
+        res.json({ success: true, message: "Student verified" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: "Server error" });
+    }
+}
+
+export const cancelStudentVerification = async (req, res) => {
+    const { studentId } = req.body;
+    
+    try {
+         await turso.execute(
+            'DELETE FROM students WHERE studentId = ?',
+            [studentId]
+         );
+
+        res.json({ success: true, message: "Student deleted" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: "Server error" });
+    }
+}
+
+export const saveStudentDetails = async (req, res)=> {
+	
+	try{
+		const { studentId, rollno } = req.body
+		
+		await turso.execute(`
+			update students set rollno = ? where studentId = ?
+		`, [rollno, studentId])
+		
+		res.json({
+			success: true,
+			message: 'Student details saved successfully'
+		})
+		
+		
+	}catch(err){
+		console.error("Error while saving student details")
+		res.status(500).json({ message: "Internal Error: while saving student details", success: false })
+	}
+
+	
+
+}

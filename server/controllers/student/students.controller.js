6db40db6c3652f62851c11aa7b0e14bdc395a10d
CasComputer <229486028+casComputer@users.noreply.github.com>
2025-11-22 10:00:24 +0530

    /verifyStudent

---
+++ server/controllers/student/students.controller.js
@@ -0,0 +1,87 @@
+import { turso } from "../../config/turso.js";
+import { validateCourseAndYear } from "../../utils/validateCourseAndYear.js";
+
+export const fetchStudentsByClass = async (req, res) => {
+    const { course, year } = req.body;
+
+    try {
+        if (!validateCourseAndYear(course, year))
+            return res
+                .status(405)
+                .json({ message: "invalid course or year", success: false });
+
+        const result = await turso.execute(
+            "SELECT studentId, fullname from students where course = ? and year_of_study = ?",
+            [course, year]
+        );
+
+        const students = result?.rows || [];
+
+        res.json({
+            students,
+            numberOfStudents: students?.length || 0,
+            success: true
+        });
+    } catch (err) {
+        console.error("Error while fetching student details: ", err);
+        res.status(500).json({
+            error: "Error while fetching student details",
+            success: false
+        });
+    }
+};
+
+export const fetchStudentsByClassTeacher = async (req, res) => {
+    const { teacherId } = req.body;
+
+    try {
+        const classResult = await turso.execute(
+            `
+            SELECT course, year 
+            FROM classes
+            WHERE in_charge = ?
+            `,
+            [teacherId]
+        );
+
+        if (classResult.rows.length === 0) {
+            return res.json({
+                success: false,
+                message: "Teacher is not assigned to any class!"
+            });
+        }
+
+        const { course, year } = classResult.rows[0];
+
+        const studentResult = await turso.execute(
+            `
+            SELECT studentId, fullname , is_verified 
+            FROM students
+            WHERE course = ? AND year_of_study = ?
+            `,
+            [course, year]
+        );
+
+        if (studentResult.rows.length === 0) {
+            return res.json({
+                success: false,
+                message: "Class has no students yet!",
+                course,
+                year
+            });
+        }
+
+        return res.json({
+            success: true,
+            course,
+            year,
+            students: studentResult.rows
+        });
+    } catch (err) {
+        console.error("Error while fetching student details:", err);
+        res.status(500).json({
+            success: false,
+            error: "Server error while fetching student details"
+        });
+    }
+}

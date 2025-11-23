117155c5e85afce824c477b047e1968c1cbbd449
casComputer <229486028+casComputer@users.noreply.github.com>
2025-11-23 11:17:41 +0530

    ..

--- server/routes/student.routes.js
+++ server/routes/student.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 
 import { turso } from "../config/turso.js";
 
-import { cancelStudentVerification, fetchStudentsByClass, fetchStudentsByClassTeacher, saveStudentDetails, verifyStudent } from "../controllers/student/students.controller.js"
+import { cancelStudentVerification, fetchStudentsByClass, fetchStudentsByClassTeacher, saveStudentDetails, verifyStudent, verifyMultipleStudents } from "../controllers/student/students.controller.js"
 
 const router = express.Router();
 
@@ -16,31 +16,7 @@ router.post('/cancelStudentVerification', cancelStudentVerification);
 
 router.post('/saveStudentDetails', saveStudentDetails)
 
-router.post('/verifyMultipleStudents', async (req, res) => {
-    try {
-        const { students } = req.body;
-
-        if (!Array.isArray(students) || students.length === 0) 
-            return res.status(400).json({ error: "students must be a non-empty array" });
-        
-        // Build placeholders: (?, ?, ?, ...)
-        const placeholders = students.map(() => '?').join(',');
-
-        const query = `
-            UPDATE students 
-            SET is_verified = true 
-            WHERE studentId IN (${placeholders})
-        `;
-
-        await turso.execute(query, students);
-
-        res.json({ success: true, message: 'students verified' });
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Internal Server Error", success: false });
-    }
-});
+router.post('/verifyMultipleStudents', verifyMultipleStudents );
 
 
 export default router;

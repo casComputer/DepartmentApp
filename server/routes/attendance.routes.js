00b8cc00ca0cce377eed4047c17ccfc5fea72b43
casComputer <229486028+casComputer@users.noreply.github.com>
2025-11-24 14:58:55 +0530

    ..

--- server/routes/attendance.routes.js
+++ server/routes/attendance.routes.js
@@ -3,5 +3,17 @@ import { turso } from "../config/turso.js";
 
 const router = express.Router();
 
+router.post('/save', async(req, res)=>{
+
+	try{
+		const { attendence, course, year } = req.body
+		
+		
+	}catch(err){
+	console.log('Error while saving attendence: ', err)
+	}
+
+})
+
 
 export default router;

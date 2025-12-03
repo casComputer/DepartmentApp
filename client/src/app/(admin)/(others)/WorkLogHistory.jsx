b513906313aa285d52475729cee1f22aa0a1a391
casDepartment <229486028+casComputer@users.noreply.github.com>
2025-12-03 11:34:30 +0530

    .

--- client/src/app/(admin)/(others)/WorkLogHistory.jsx
+++ client/src/app/(admin)/(others)/WorkLogHistory.jsx
@@ -1,101 +1,42 @@
 import { useState } from "react";
 import { View, Text, ActivityIndicator, Pressable } from "react-native";
 import { FlashList } from "@shopify/flash-list";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 import Header from "@components/common/Header.jsx";
+import UserItem from "@components/common/UserItem.jsx";
 
-import { fetchWorklogs } from "@controller/teacher/worklog.controller";
-
-const showDate = date => {
-    const formatted = new Date(date).toLocaleDateString();
-    const today = new Date();
-    if (formatted === today.toLocaleDateString()) return "Today";
-    const yes = new Date(today.setDate(today.getDate() - 1));
-    if (formatted === yes.toLocaleDateString()) return "Yesterday";
-    return formatted;
-};
-
-const RenderItem = ({ item }) => {
-    const [showFullTopics, setShowFullTopics] = useState(false);
-
-    return (
-        <View
-            style={{
-                boxShadow: "0px 2px 3px rgba(0, 0, 0, 0.5)"
-            }}
-            className="bg-white m-2 p-4 rounded-3xl dark:bg-zinc-900">
-            <View className="flex-row justify-between items-center mb-2">
-                <Text
-                    numberOfLines={2}
-                    className="text-lg font-black dark:text-white">
-                    Subject: {"\n\t"}
-                    {item.subject}
-                </Text>
-                <View className="items-end">
-                    <Text className="text-lg font-black dark:text-white">
-                        {showDate(item.date)}
-                    </Text>
-                    <Text className="text-lg font-black dark:text-white">
-                        {item.year} {item.course}
-                    </Text>
-                    <Text className="text-lg font-black dark:text-white">
-                        {item.hour} Hour
-                    </Text>
-                </View>
-            </View>
-
-            <Text className="text-xl font-black mt-5 dark:text-white">
-                Topics:
-            </Text>
-
-            <Text
-                numberOfLines={showFullTopics ? undefined : 1}
-                className="text-xl pl-3 dark:text-white max-w-[90%]">
-                {item.topics}
-            </Text>
-            <Pressable onPress={() => setShowFullTopics(prev => !prev)}>
-                <Text className="text-sm pl-3 font-bold text-blue-400">
-                    {showFullTopics ? "less" : "more"}
-                </Text>
-            </Pressable>
-        </View>
-    );
-};
+import { fetchTeachers } from "@controller/admin/teachers.controller.js";
+import { router } from "expo-router";
 
 const WorkLogHistory = () => {
-    const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
-        useInfiniteQuery({
-            queryKey: ["worklogs"],
-            queryFn: ({ pageParam = 1 }) => fetchWorklogs(pageParam),
-            getNextPageParam: lastPage =>
-                lastPage.hasMore ? lastPage.nextPage : undefined
-        });
+  const { data: teachers } = useQuery({
+    queryKey: ["teachers"],
+    queryFn: () => fetchTeachers(),
+  });
 
-    return (
-        <View className="flex-1 bg-white dark:bg-black">
-            <Header title={"History"} />
+  return (
+    <View className="flex-1 bg-white dark:bg-black">
+      <Header title={"History"} />
 
-            <FlashList
-                data={data?.pages?.flatMap(page => page?.data)}
-                keyExtractor={item => item.id}
-                renderItem={({ item }) => <RenderItem item={item} />}
-                endReachedThreshold={0.5}
-                onEndReached={() => {
-                    if (hasNextPage && !isFetchingNextPage) fetchNextPage();
-                }}
-                contentContainerStyle={{
-                    paddingBottom: 60,
-                    paddingHorizontal: 5
-                }}
-                ListFooterComponent={
-                    isFetchingNextPage ? (
-                        <ActivityIndicator size="large" />
-                    ) : null
-                }
-            />
-        </View>
-    );
+      <FlashList
+        data={teachers}
+        renderItem={({ item }) => (
+          <UserItem
+            item={item}
+            showVerification={false}
+            handlePress={() =>
+              router.push({
+                pathname: "/(admin)/(others)/WorkLogDetails",
+                params: { teacherId: item.teacherId },
+              })
+            }
+          />
+        )}
+        contentContainerStyle={{ padding: 10 }}
+      />
+    </View>
+  );
 };
 
 export default WorkLogHistory;
--- client/src/app/(admin)/(tabs)/Profile.jsx
+++ client/src/app/(admin)/(tabs)/Profile.jsx
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 
+import { useAppStore } from "@store/app.store.js";
+
+const removeUser = useAppStore.getState().removeUser
 const Profile = () => {
   return (
     <View className="flex-1 justify-center items-center bg-blue-700">
       <Text>index</Text>
+      <Button title ='logout' onPress={()=> removeUser()}/>
     </View>
   )
 }

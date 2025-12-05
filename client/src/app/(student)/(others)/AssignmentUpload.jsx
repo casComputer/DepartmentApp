3270613c7741d4be508266f05e54db5d2fab05a2
casDepartment <229486028+casComputer@users.noreply.github.com>
2025-12-05 14:10:40 +0530

    ..

--- client/src/app/(student)/(others)/AssignmentUpload.jsx
+++ client/src/app/(student)/(others)/AssignmentUpload.jsx
@@ -1,24 +1,33 @@
-import { View, Text } from "react-native";
-// import * as DocumentPicker from "expo-document-picker";
-
-import Header from "@components/common/Header2.jsx";
+import { View, Text, TouchableOpacity } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 
+import { handleDocumentPick } from "@controller/student/assignment.controller.js";
+
+import Header from "@components/common/Header2.jsx";
+
 const AssignmentUpload = () => {
-  const { assignmentId, topic, description } = useLocalSearchParams();
+  const { assignmentId, topic, description, dueDate } = useLocalSearchParams();
 
   return (
     <View className="flex-1 bg-white dark:bg-black">
       <Header />
 
       <View className="p-5">
-        <Text className="text-3xl font-black dark:text-white">
+        <Text className="text-3xl font-black dark:text-white mt-5">
           Topic: {topic}
         </Text>
-        <Text className="text-lg mb-6 mt-3 dark:text-white">
+        <Text className="text-lg mt-3 dark:text-zinc-400">
           Description: {description}
         </Text>
+        <Text className="text-lg mb-6 mt-3 dark:text-white">
+          Due Date: {new Date(dueDate).toLocaleDateString()}
+        </Text>
         {/* Add your file upload component or logic here */}
+        <TouchableOpacity className="" onPress={handleDocumentPick}>
+          <Text className="text-blue-500 text-center font-bold text-3xl mt-5">
+            Select File
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );

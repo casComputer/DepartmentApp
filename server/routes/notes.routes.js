a08e59c7c1fc02da175520230c273a23b2e08aa5
casComputer <229486028+casComputer@users.noreply.github.com>
2025-12-15 20:35:56 +0530

    notes fetch endpoint for teachers

--- server/routes/notes.routes.js
+++ server/routes/notes.routes.js
@@ -1,16 +1,28 @@
 import express from "express";
 
+import Notes from "../models/notes.js";
+
 const router = express.Router();
 
-router.post('/fetchNotesForTeacher', async(req, res)=>{
+router.post("/fetchByTeacher", async (req, res) => {
     try {
-      
+        const { teacherId } = req.body
+        
+        if(!teacherId) return res.json({ success: false, message: 'teacher id is required!'})
+        
+        const notes = await Notes.find({
+            parentId: null,
+            teacherId
+        });
+        
+        res.json({ notes, success: true})
     } catch (error) {
-      console.error(error);
-      res.send(500).json({
-          success: false, message: "Internal server error!"
-      })
+        console.error(error);
+        res.send(500).json({
+            success: false,
+            message: "Internal server error!"
+        });
     }
-})
+});
 
 export default router;

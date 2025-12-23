6c341419b15e469c95963bc94cd27d32ad1a50bf
AdwaithAnandSR <181568233+AdwaithAnandSR@users.noreply.github.com>
2025-12-23 11:20:33 +0530

    working on sync teacher details

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -7,87 +7,88 @@ import { storage, clearUser } from "./storage";
 // const url = process.env.EXPO_PUBLIC_API_BASE_URL;
 
 // const url = "http://192.168.20.62:3000";
-const url = "http://192.168.20.90:3000";
+const url = "http://192.168.20.75:3000";
+
 // const url = "https://dc-connect.onrender.com";
 
 console.log(url);
 
 const api = axios.create({
-  baseURL: url,
+	baseURL: url,
 });
 
 api.interceptors.request.use(
-  async (config) => {
-    const token = storage.getString("accessToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
+	async (config) => {
+		const token = storage.getString("accessToken");
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(error) => Promise.reject(error)
 );
 
 api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalReq = error.config;
+	(response) => response,
+	async (error) => {
+		const originalReq = error.config;
 
-    if (
-      (error.response?.status === 403 || error.response?.status === 401) &&
-      !originalReq._retry
-    ) {
-      originalReq._retry = true;
+		if (
+			(error.response?.status === 403 || error.response?.status === 401) &&
+			!originalReq._retry
+		) {
+			originalReq._retry = true;
 
-      try {
-        const refreshToken = await SecureStore.getItemAsync("refreshToken");
-        if (!refreshToken) {
-          storage.remove("accessToken");
-          await SecureStore.deleteItemAsync("refreshToken");
-          clearUser();
-          router.replace("/auth/Signin");
-        }
+			try {
+				const refreshToken = await SecureStore.getItemAsync("refreshToken");
+				if (!refreshToken) {
+					storage.remove("accessToken");
+					await SecureStore.deleteItemAsync("refreshToken");
+					clearUser();
+					router.replace("/auth/Signin");
+				}
 
-        const { data } = await axios.post(`${url}/auth/refresh`, {
-          refreshToken,
-        });
+				const { data } = await axios.post(`${url}/auth/refresh`, {
+					refreshToken,
+				});
 
-        storage.set("accessToken", data.accessToken);
+				storage.set("accessToken", data.accessToken);
 
-        // ======= temporary removed token rotaion =======
-        // await SecureStore.setItemAsync(
-        //     "refreshToken",
-        //     data.refreshToken,
-        //     {
-        //         keychainAccessible: SecureStore.WHEN_UNLOCKED
-        //     }
-        // );
+				// ======= temporary removed token rotaion =======
+				// await SecureStore.setItemAsync(
+				//     "refreshToken",
+				//     data.refreshToken,
+				//     {
+				//         keychainAccessible: SecureStore.WHEN_UNLOCKED
+				//     }
+				// );
 
-        originalReq.headers.Authorization = `Bearer ${data.accessToken}`;
+				originalReq.headers.Authorization = `Bearer ${data.accessToken}`;
 
-        return api(originalReq);
-      } catch (err) {
-        console.error("Refresh error:", err.response?.status, err.message);
+				return api(originalReq);
+			} catch (err) {
+				console.error("Refresh error:", err.response?.status, err.message);
 
-        // Only logout if refresh token is invalid/expired
-        const status = err.response?.status;
+				// Only logout if refresh token is invalid/expired
+				const status = err.response?.status;
 
-        if (status === 401 || status === 403) {
-          console.log("Refresh token invalid → logging out");
-          storage.remove("accessToken");
-          await SecureStore.deleteItemAsync("refreshToken");
-          clearUser();
-          router.replace("/auth/Signin");
-          return Promise.reject(err);
-        }
+				if (status === 401 || status === 403) {
+					console.log("Refresh token invalid → logging out");
+					storage.remove("accessToken");
+					await SecureStore.deleteItemAsync("refreshToken");
+					clearUser();
+					router.replace("/auth/Signin");
+					return Promise.reject(err);
+				}
 
-        // Otherwise server/network error
-        console.log("Server or network issue → do NOT log out");
-        return Promise.reject(err);
-      }
-    }
+				// Otherwise server/network error
+				console.log("Server or network issue → do NOT log out");
+				return Promise.reject(err);
+			}
+		}
 
-    return Promise.reject(error);
-  }
+		return Promise.reject(error);
+	}
 );
 
 export default api;

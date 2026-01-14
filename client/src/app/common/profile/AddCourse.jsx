749b62716adb01695899fb7e3f8fba284a10abe8
casComputer <229486028+casComputer@users.noreply.github.com>
2026-01-14 17:59:31 +0530

    deletypo

--- client/src/app/common/profile/AddCourse.jsx
+++ client/src/app/common/profile/AddCourse.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect, useRef, use } from "react";
 import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  ToastAndroid,
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    ToastAndroid
 } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-controller";
 import { Entypo } from "@icons";
@@ -20,126 +20,158 @@ import { save } from "@controller/teacher/course.controller.js";
 import { COURSES, YEAR } from "@constants/ClassAndCourses.js";
 
 const AddCourse = () => {
-  const [year, setYear] = useState(null);
-  const [course, setCourse] = useState(null);
-  const [courseName, setCourseName] = useState(null);
-  const [list, setList] = useState(
-    useAppStore
-      .getState()
-      .user?.courses.map((item, index) => ({
-        ...item,
-        id: index + 1,
-        courseName: item.course_name,
-      })) || []
-  );
+    const [year, setYear] = useState(null);
+    const [course, setCourse] = useState(null);
+    const [courseName, setCourseName] = useState(null);
+    const [saving, setSaving] = useState(false);
 
-  const inputRef = useRef(null);
+    const [list, setList] = useState(
+        useAppStore.getState().user?.courses.map((item, index) => ({
+            ...item,
+            id: index + 1,
+            courseName: item.course_name
+        })) || []
+    );
 
-  const checkFields = () => {
-    if (!year || !course || !courseName) {
-      ToastAndroid.show(
-        "Please fill all the field before continuing...",
-        ToastAndroid.SHORT
-      );
-      return false;
-    }
-    return true;
-  };
+    const inputRef = useRef(null);
 
-  const handleSave = () => {
-    if (!list || !list.length)
-      return ToastAndroid.show("No course added!", ToastAndroid.SHORT);
+    const checkFields = () => {
+        if (!year || !course || !courseName) {
+            ToastAndroid.show(
+                "Please fill all the field before continuing...",
+                ToastAndroid.SHORT
+            );
+            return false;
+        }
+        return true;
+    };
 
-    save({ list });
-  };
+    const handleAdd = async () => {
+        if (!checkFields()) return;
+        await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
 
-  const handleAdd = () => {
-    if (!checkFields()) return;
-    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+        setList(prev => [
+            ...prev,
+            {
+                course: course?.id ?? course,
+                year: year?.id ?? year,
+                courseName: courseName?.trim().toUpperCase(),
+                id: prev?.length
+            }
+        ]);
+        setCourseName("");
+        setCourse(null);
+        setYear(null);
+    };
 
-    setList((prev) => [
-      ...prev,
-      {
-        course,
-        year,
-        courseName: courseName?.trim().toUpperCase(),
-        id: prev?.length,
-      },
-    ]);
-    setCourseName("");
-    setCourse(null);
-    setYear(null);
-  };
+    const handleSave = async () => {
+        let finalList = list;
 
-  const handleRemove = (id) => {
-    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-    setList((prev) => [...prev.filter((item) => item.id !== id)]);
-  };
+        if (year && course && courseName) {
+            finalList = [
+                ...list,
+                {
+                    course: course?.id ?? course,
+                    year: year?.id ?? year,
+                    courseName: courseName.trim().toUpperCase(),
+                    id: list.length
+                }
+            ];
 
-  useEffect(() => {
-    if (course && year && course?.id && year?.id) inputRef?.current?.focus();
-  }, [course, year, inputRef]);
+            setList(finalList);
+            setCourseName("");
+            setCourse(null);
+            setYear(null);
+        }
 
-  return (
-    <KeyboardAwareScrollView
-      showsVerticalScrollIndicator={false}
-      keyboardShouldPersistTaps="handled"
-      contentContainerStyle={{ flexGrow: 1 }}
-      style={{ paddingBottom: 100 }}
-      className="bg-primary"
-    >
-      <Header title={"Add Course"} />
-      <View className="px-2">
-        <Select title="Year" options={YEAR} selected={year} select={setYear} />
-        <Select
-          title="Course"
-          options={COURSES}
-          selected={course}
-          select={setCourse}
-        />
-        <TextInput
-          ref={inputRef}
-          className="mt-4 px-4 py-5 rounded-3xl text-text font-bold border border-text-secondary"
-          value={courseName}
-          placeholder={"enter course name"}
-          onSubmitEditing={handleAdd}
-          onChangeText={setCourseName}
-        />
-        <View className="flex-wrap flex-row items-center gap-2 mt-5">
-          {list?.map((item) => (
-            <View
-              key={`${item.id}-${item.courseName}`}
-              className="px-2 py-2 bg-pink-600 rounded-full flex-row justify-between max-w-full items-center gap-1 overflow-hidden"
-            >
-              <TouchableOpacity onPress={() => handleRemove(item.id)}>
-                <Entypo name="cross" size={22} />
-              </TouchableOpacity>
+        if (!finalList.length) {
+            ToastAndroid.show("No course added!", ToastAndroid.SHORT);
+            return;
+        }
 
-              <Text
-                numberOfLines={1}
-                className="max-w-full text-text font-bold text-sm text-center"
-              >
-                {item.year.id ?? item.year} {item.course.id ?? item.course}{" "}
-                {item.courseName}
-              </Text>
+        setSaving(true);
+        await save({ list: finalList });
+        setSaving(false);
+    };
+
+    const handleRemove = id => {
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+        setList(prev => [...prev.filter(item => item.id !== id)]);
+    };
+
+    useEffect(() => {
+        if (course && year && course?.id && year?.id)
+            inputRef?.current?.focus();
+    }, [course, year, inputRef]);
+
+    return (
+        <KeyboardAwareScrollView
+            showsVerticalScrollIndicator={false}
+            keyboardShouldPersistTaps="handled"
+            contentContainerStyle={{ flexGrow: 1 }}
+            style={{ paddingBottom: 100 }}
+            className="bg-primary"
+        >
+            <Header title={"Add Course"} />
+            <View className="px-2">
+                <Select
+                    title="Year"
+                    options={YEAR}
+                    selected={year}
+                    select={setYear}
+                />
+                <Select
+                    title="Course"
+                    options={COURSES}
+                    selected={course}
+                    select={setCourse}
+                />
+                <TextInput
+                    ref={inputRef}
+                    className="mt-4 px-4 py-5 rounded-3xl text-text font-bold border border-text-secondary"
+                    value={courseName}
+                    placeholder={"enter course name"}
+                    onSubmitEditing={handleAdd}
+                    onChangeText={setCourseName}
+                />
+                <View className="flex-wrap flex-row items-center gap-2 mt-5">
+                    {list?.map(item => (
+                        <View
+                            key={`${item.id}-${item.courseName}`}
+                            className="px-2 py-2 bg-pink-600 rounded-full flex-row justify-between max-w-full items-center gap-1 overflow-hidden"
+                        >
+                            <TouchableOpacity
+                                onPress={() => handleRemove(item.id)}
+                            >
+                                <Entypo name="cross" size={22} />
+                            </TouchableOpacity>
+
+                            <Text
+                                numberOfLines={1}
+                                className="max-w-full text-text font-bold text-sm text-center"
+                            >
+                                {item.year?.id ?? item.year}{" "}
+                                {item.course?.id ?? item.course}{" "}
+                                {item.courseName}
+                            </Text>
+                        </View>
+                    ))}
+                </View>
             </View>
-          ))}
-        </View>
-      </View>
-      <View className="flex-1 justify-end py-8">
-        <TouchableOpacity onPress={handleAdd}>
-          <Text className="mt-8 text-2xl font-bold text-blue-500 text-center my-3">
-            Add Another
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handleSave}>
-          <Text className="text-2xl font-bold text-green-500 text-center my-3">
-            Save
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAwareScrollView>
-  );
+            <View className="flex-1 justify-end py-8">
+                <TouchableOpacity onPress={handleAdd}>
+                    <Text className="mt-8 text-2xl font-bold text-blue-500 text-center my-3">
+                        Add Another
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={handleSave}>
+                    <Text className="text-2xl font-bold text-green-500 text-center my-3">
+                        {saving ? "Saving.." : "Save"}
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAwareScrollView>
+    );
 };
 
 export default AddCourse;

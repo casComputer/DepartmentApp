09258243cecfa31f5e656dde0ccd68968172b98a
casComputer <229486028+casComputer@users.noreply.github.com>
2026-01-18 14:04:04 +0530

    ..

--- client/src/controller/student/attendance.controller.js
+++ client/src/controller/student/attendance.controller.js
@@ -8,7 +8,7 @@ export const getTodaysAttendanceReport = async () => {
         const userId = useAppStore.getState().user?.userId;
         if (!userId) return;
 
-        const res = await axios.post("/student/getTodaysAttendanceReport", {
+        const res = await axios.post("/attendance/getTodaysAttendanceReport", {
             userId
         });
 
@@ -30,24 +30,26 @@ export const getTodaysAttendanceReport = async () => {
     }
 };
 
-export const getMonthlyAttendenceMiniReport = async () => {
+export const getOverallAttendenceReport = async () => {
     try {
         const userId = useAppStore.getState().user?.userId;
         if (!userId) return;
 
         const res = await axios.post(
-            "/student/getMonthlyAttendanceMiniReport",
+            "/attendance/overallAttendenceReport",
             {
                 userId
             }
         );
+        
+        console.log(res.data)
 
         if (res.data.success) return res.data.report;
         else {
             return res.data.report;
         }
     } catch (error) {
-        console.error(error);
+        console.error(error.message);
         ToastAndroid.show(
             `Failed to fetching monthly attendance report!`,
             ToastAndroid.LONG
@@ -60,7 +62,7 @@ export const getYearlyAttendenceReport = async year => {
     try {
         if (!year) return ToastAndroid.show("invalid year", ToastAndroid.SHORT);
 
-        const res = await axios.post("/student/getYearlyAttendanceReport", {
+        const res = await axios.post("/attendance/getYearlyAttendanceReport", {
             year: year?.toString()
         });
 
@@ -120,7 +122,7 @@ export const generateAttendanceCalendarReport = async (month, year) => {
         if (!userId) return;
 
         const res = await axios.post(
-            "/student/generateAttendanceCalendarReport",
+            "/attendance/generateAttendanceCalendarReport",
             {
                 userId,
                 month,

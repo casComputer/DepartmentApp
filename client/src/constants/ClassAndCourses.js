efaa36278b3823a0215a03c89037f4aaf074cc43
casComputer <229486028+casComputer@users.noreply.github.com>
2026-01-20 16:18:42 +0530

    ..

--- client/src/constants/ClassAndCourses.js
+++ client/src/constants/ClassAndCourses.js
@@ -55,3 +55,47 @@ export const HOURS = [
         key: 5
     }
     ]
+
+
+export const SEM = [
+        {
+        id: "First",
+        title: "First",
+        key: 1
+    },
+    {
+        id: "Second",
+        title: "Second",
+        key: 2
+    },
+    {
+        id: "Third",
+        title: "Third",
+        key: 3
+    },
+    {
+        id: "Fourth",
+        title: "Fourth",
+        key: 4
+    },
+    {
+        id: "Fifth",
+        title: "Fifth",
+        key: 5
+    },
+    {
+        id: "Sixth",
+        title: "Sixth",
+        key: 6
+    },
+    {
+        id: "Seventh",
+        title: "Seventh",
+        key: 7
+    },
+    {
+        id: "Eight",
+        title: "Eight",
+        key: 8
+    },
+    ]

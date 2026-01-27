fd134ee2b53b8c42a65abe5c1005dbe9de7d2b25
casComputer <229486028+casComputer@users.noreply.github.com>
2026-01-27 21:04:48 +0530

    typo

--- client/src/app/(admin)/(others)/ManageTeachers.jsx
+++ client/src/app/(admin)/(others)/ManageTeachers.jsx
@@ -1,22 +1,40 @@
-import { useState, useEffect } from "react";
-import { View, Text, ActivityIndicator } from "react-native";
-import { FlashList } from "@shopify/flash-list";
-import { router } from "expo-router";
+import {
+    useState,
+    useEffect
+} from "react";
+import {
+    View,
+    Text,
+    ActivityIndicator
+} from "react-native";
+import {
+    FlashList
+} from "@shopify/flash-list";
+import {
+    router
+} from "expo-router";
 
-import { fetchTeachers } from "@controller/admin/teachers.controller.js";
-import { useAdminStore } from "@store/admin.store.js";
+import {
+    fetchTeachers
+} from "@controller/admin/teachers.controller.js";
+import {
+    useAdminStore
+} from "@store/admin.store.js";
 
 import TeacherItem from "@components/common/UserItem.jsx";
 import Header from "@components/common/Header.jsx";
 
 const ManageTeachers = () => {
     const teachers = useAdminStore(state => state.teachers);
-    const [loading, setLoading] = useState(false);
+    const [loading,
+        setLoading] = useState(false);
 
     useEffect(() => {
         setLoading(true);
         fetchTeachers().then(() => setLoading(false));
     }, []);
+    
+    console.log(teachers)
 
     return (
         <View className="flex-1 bg-primary">
@@ -25,25 +43,26 @@ const ManageTeachers = () => {
                 <ActivityIndicator size="large" />
             )}
             <FlashList
-                data={teachers}
+                data={teachers ??[]}
                 showsVerticalScrollIndicator={false}
                 keyExtractor={(item, index) =>
-                    item.teacherId.toString() ?? index
+                item?.teacherId?.toString() ?? index
                 }
                 className={"px-2"}
-                contentContainerStyle={{ paddingBottom: 60 }}
+                contentContainerStyle={ { paddingBottom: 60 }}
                 renderItem={({ item }) => (
                     <TeacherItem
                         item={item}
                         handlePress={() =>
-                            router.push({
-                                pathname: "(admin)/(others)//VerifyTeacher",
-                                params: { teacherId: item.teacherId }
-                            })
+                        item?.teacherId &&
+                        router.push({
+                            pathname: "(admin)/(others)//VerifyTeacher",
+                            params: { teacherId: item?.teacherId }
+                        })
                         }
-                    />
+                        />
                 )}
-            />
+                />
         </View>
     );
 };

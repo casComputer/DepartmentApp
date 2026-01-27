d7d42105ffdd3fd801b511668f8d09504d43bde1
casComputer <229486028+casComputer@users.noreply.github.com>
2026-01-27 21:34:38 +0530

    typo on sync students for attendance

--- client/src/app/(admin)/(others)/WorkLogDetails.jsx
+++ client/src/app/(admin)/(others)/WorkLogDetails.jsx
@@ -1,108 +1,136 @@
-import { useState } from "react";
-import { View, Text, ActivityIndicator, Pressable } from "react-native";
-import { FlashList } from "@shopify/flash-list";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { useLocalSearchParams } from "expo-router";
+import {
+    useState
+} from "react";
+import {
+    View,
+    Text,
+    ActivityIndicator,
+    Pressable
+} from "react-native";
+import {
+    FlashList
+} from "@shopify/flash-list";
+import {
+    useInfiniteQuery
+} from "@tanstack/react-query";
+import {
+    useLocalSearchParams
+} from "expo-router";
 
 import Header from "@components/common/Header.jsx";
 
-import { fetchWorklogs } from "@controller/admin/worklog.controller";
+import {
+    fetchWorklogs
+} from "@controller/admin/worklog.controller";
 
 const showDate = (date) => {
-	const formatted = new Date(date).toLocaleDateString();
-	const today = new Date();
-	if (formatted === today.toLocaleDateString()) return "Today";
-	const yes = new Date(today.setDate(today.getDate() - 1));
-	if (formatted === yes.toLocaleDateString()) return "Yesterday";
-	return formatted;
+    const formatted = new Date(date).toLocaleDateString();
+    const today = new Date();
+    if (formatted === today.toLocaleDateString()) return "Today";
+    const yes = new Date(today.setDate(today.getDate() - 1));
+    if (formatted === yes.toLocaleDateString()) return "Yesterday";
+    return formatted;
 };
 
-const RenderItem = ({ item }) => {
-	const [showFullTopics, setShowFullTopics] = useState(false);
+const RenderItem = ({
+    item
+}) => {
+    const [showFullTopics,
+        setShowFullTopics] = useState(false);
 
-	return (
-		<View
-			style={{
-				boxShadow: "0px 2px 3px rgba(0, 0, 0, 0.5)",
-			}}
-			className="bg-card m-2 p-4 rounded-3xl"
-		>
-			<View className="flex-row justify-between items-center mb-2">
-				<Text numberOfLines={2} className="text-lg font-black dark:text-white">
-					Subject: {"\n\t"}
-					{item.subject}
-				</Text>
-				<View className="items-end">
-					<Text className="text-lg font-black dark:text-white">
-						{showDate(item.date)}
-					</Text>
-					<Text className="text-lg font-black dark:text-white">
-						{item.year} {item.course}
-					</Text>
-					<Text className="text-lg font-black dark:text-white">
-						{item.hour} Hour
-					</Text>
-				</View>
-			</View>
+    return (
+        <View
+            style={ {
+                boxShadow: "0px 2px 3px rgba(0, 0, 0, 0.5)",
+            }}
+            className="bg-card m-2 p-4 rounded-3xl"
+            >
+            <View className="flex-row justify-between items-center mb-2">
+                <Text numberOfLines={2} className="text-lg font-black dark:text-white">
+                    Subject: {"\n\t"}
+                    {item.subject}
+                </Text>
+                <View className="items-end">
+                    <Text className="text-lg font-black dark:text-white">
+                        {showDate(item.date)}
+                    </Text>
+                    <Text className="text-lg font-black dark:text-white">
+                        {item.year} {item.course}
+                    </Text>
+                    <Text className="text-lg font-black dark:text-white">
+                        {item.hour} Hour
+                    </Text>
+                </View>
+            </View>
 
-			<Text className="text-xl font-black mt-5 dark:text-white">Topics:</Text>
+            <Text className="text-xl font-black mt-5 dark:text-white">Topics:</Text>
 
-			<Text
-				numberOfLines={showFullTopics ? undefined : 1}
-				className="text-xl pl-3 dark:text-white max-w-[90%]"
-			>
-				{item.topics}
-			</Text>
-			<Pressable onPress={() => setShowFullTopics((prev) => !prev)}>
-				<Text className="text-sm pl-3 font-bold text-blue-400">
-					{showFullTopics ? "less" : "more"}
-				</Text>
-			</Pressable>
-		</View>
-	);
+            <Text
+                numberOfLines={showFullTopics ? undefined: 1}
+                className="text-xl pl-3 dark:text-white max-w-[90%]"
+                >
+                {item.topics}
+            </Text>
+            <Pressable onPress={() => setShowFullTopics((prev) => !prev)}>
+                <Text className="text-sm pl-3 font-bold text-blue-400">
+                    {showFullTopics ? "less": "more"}
+                </Text>
+            </Pressable>
+        </View>
+    );
 };
 
 const WorkLogHistory = () => {
-	const { teacherId } = useLocalSearchParams();
+    const {
+        teacherId
+    } = useLocalSearchParams();
 
-	const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
-		useInfiniteQuery({
-			queryKey: ["worklogs", teacherId],
-			queryFn: ({ pageParam = 1 }) => fetchWorklogs(pageParam, teacherId),
-			getNextPageParam: (lastPage) =>
-				lastPage.hasMore ? lastPage.nextPage : undefined,
-		});
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+        isLoading
+    } =
+    useInfiniteQuery( {
+        queryKey: ["worklogs", teacherId],
+        queryFn: ({
+            pageParam = 1
+        }) => fetchWorklogs(pageParam, teacherId),
+        getNextPageParam: (lastPage) =>
+        lastPage.hasMore ? lastPage.nextPage: undefined,
+    });
 
-	return (
-		<View className="flex-1 bg-primary">
-			<Header title={"History"} />
+    return (
+        <View className="flex-1 bg-primary">
+            <Header title={"History"} />
 
-			{isLoading && <ActivityIndicator size={"large"} />}
+            {isLoading && <ActivityIndicator size={"large"} />}
 
-			<FlashList
-				data={data?.pages?.flatMap((page) => page?.data)}
-				keyExtractor={(item) => item.id}
-				renderItem={({ item }) => <RenderItem item={item} />}
-				endReachedThreshold={0.5}
-				onEndReached={() => {
-					if (hasNextPage && !isFetchingNextPage) fetchNextPage();
-				}}
-				contentContainerStyle={{
-					paddingBottom: 60,
-					paddingHorizontal: 5,
-				}}
-				ListFooterComponent={
-					isFetchingNextPage ? <ActivityIndicator size="large" /> : null
-				}
-				ListEmptyComponent={
-					<Text className="text-text mt-5 font-bold text-center">
-						No data available for this teacher!
-					</Text>
-				}
-				showsVerticalScrollIndicator={false}
-			/>
-		</View>
-	);
+            <FlashList
+                data={data?.pages?.flatMap((page) => page?.data)}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => <RenderItem item={item} />}
+                endReachedThreshold={0.5}
+                onEndReached={() => {
+                    if (hasNextPage && !isFetchingNextPage) fetchNextPage();
+                }}
+                contentContainerStyle={ {
+                    paddingBottom: 60,
+                    paddingHorizontal: 5,
+                }}
+                ListFooterComponent={
+                isFetchingNextPage ? <ActivityIndicator size="large" />: null
+                }
+                ListEmptyComponent={
+                <Text className="text-text mt-5 font-bold text-center">
+                    No data available for this teacher!
+                </Text>
+                }
+                showsVerticalScrollIndicator={false}
+                />
+        </View>
+    );
 };
 
 export default WorkLogHistory;
--- client/src/controller/admin/teachers.controller.js
+++ client/src/controller/admin/teachers.controller.js
@@ -1,8 +1,14 @@
 import axios from "@utils/axios.js";
-import { router } from "expo-router";
+import {
+    router
+} from "expo-router";
 
-import { useAdminStore } from "@store/admin.store.js";
-import { ToastAndroid } from "react-native";
+import {
+    useAdminStore
+} from "@store/admin.store.js";
+import {
+    ToastAndroid
+} from "react-native";
 
 export const fetchTeachers = async () => {
     try {
@@ -18,7 +24,9 @@ export const fetchTeachers = async () => {
     }
 };
 
-export const assignClass = async ({ year, course, teacherId }) => {
+export const assignClass = async ({
+    year, course, teacherId
+}) => {
     try {
         const res = await axios.post("/admin/assignClass", {
             year: year.id,
@@ -32,9 +40,9 @@ export const assignClass = async ({ year, course, teacherId }) => {
             router.back();
         } else
             ToastAndroid.show(
-                res.data.message ?? "failed to assign class",
-                ToastAndroid.LONG
-            );
+            res.data.message ?? "failed to assign class",
+            ToastAndroid.LONG
+        );
     } catch (error) {
         console.error(error);
         ToastAndroid.show("failed to assign class", ToastAndroid.LONG);
@@ -43,7 +51,9 @@ export const assignClass = async ({ year, course, teacherId }) => {
 
 export const verifyTeacher = async (teacherId) => {
     try {
-        const res = await axios.post("/admin/verifyTeacher", { teacherId });
+        const res = await axios.post("/admin/verifyTeacher", {
+            teacherId
+        });
 
         if (res.data.success) {
             const verifyTeacher = useAdminStore.getState().verifyTeacher;
@@ -56,7 +66,9 @@ export const verifyTeacher = async (teacherId) => {
 
 export const cancelVerification = async (teacherId) => {
     try {
-        const res = await axios.post("/admin/deleteTeacher", { teacherId });
+        const res = await axios.post("/admin/deleteTeacher", {
+            teacherId
+        });
 
         return res.data.success;
     } catch (error) {

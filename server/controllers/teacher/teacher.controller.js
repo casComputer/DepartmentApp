6a4f478fb58ad5dc2f314ad1d3c1903bbcc19c63
casComputer <229486028+casComputer@users.noreply.github.com>
2026-01-31 09:53:59 +0530

    p

--- server/controllers/teacher/teacher.controller.js
+++ server/controllers/teacher/teacher.controller.js
@@ -54,13 +54,16 @@ export const fetchAllTeachers = async (req, res) => {
             u.userId as teacherId,
             u.fullname,
             u.dp,
+            u.phone,
+            u.about,
+            u.email
 
             tc.course,
             tc.year,
             tc.course_name,
 
             c.course as in_charge_course,
-            c.year as in_charge_year
+            c.year as in_charge_year,
 
             FROM users u
 

bfddaaf343eb75ab86a71340b7f5c504d2406dcc
casComputer <229486028+casComputer@users.noreply.github.com>
2026-02-01 21:18:37 +0530

    delete report route

--- server/routes/dashboard.routes.js
+++ server/routes/dashboard.routes.js
@@ -1,31 +1,32 @@
 import express from "express";
 
+import cloudinary from "../config/cloudinary.js"
+
 const router = express.Router();
 
 router.get("/cloudinary", async (req, res) => {
     try {
         const usage = await cloudinary.api.usage();
-        
-        const folders = await cloudinary.api.root_folders();
-        
-        const images = await cloudinary.api.resources({
-  resource_type: "image",
-  max_results: 1
-});
 
-const videos = await cloudinary.api.resources({
-  resource_type: "video",
-  max_results: 1
-});
-        
+        const folders = await cloudinary.api.root_folders();
+
+        const images = await cloudinary.api.resources({
+            resource_type: "image",
+            max_results: 1
+        });
+
+        const videos = await cloudinary.api.resources({
+            resource_type: "video",
+            max_results: 1
+        });
+
         res.json({
             success: true,
             usage,
             folders,
             images,
-            videos,
-            
-        })
+            videos
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({

88c1a37808fa6c27b6be50615cdaef556f71912b
casComputer <229486028+casComputer@users.noreply.github.com>
2026-02-02 08:06:50 +0530

    ..

--- server/config/turso.js
+++ server/config/turso.js
@@ -1,11 +1,19 @@
 import "dotenv/config";
 
 import { createClient } from "@libsql/client/web";
+import { createClient as statsClient } from "@tursodatabase/api";
 
 const PRIMARY_DATABASE_URL = process.env.PRIMARY_DATABASE_URL;
 const PRIMARY_DATABASE_TOKEN = process.env.PRIMARY_DATABASE_TOKEN;
+const PRIMARY_DATABASE_ADMIN_TOKEN = process.env.PRIMARY_DATABASE_ADMIN_TOKEN;
+const PRIMARY_DATABASE_ORGANISATION = process.env.PRIMARY_DATABASE_ORGANISATION;
 
 export const turso = createClient({
-	url: PRIMARY_DATABASE_URL,
-	authToken: PRIMARY_DATABASE_TOKEN,
+    url: PRIMARY_DATABASE_URL,
+    authToken: PRIMARY_DATABASE_TOKEN
+});
+
+export const tursoStats = statsClient({
+    org: PRIMARY_DATABASE_ORGANISATION,
+    token: PRIMARY_DATABASE_ADMIN_TOKEN
 });

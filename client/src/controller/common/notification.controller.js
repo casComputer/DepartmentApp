9bedc340a779ab2533a03740afa8676f4c317434
casComputer <229486028+casComputer@users.noreply.github.com>
2026-02-10 11:12:08 +0530

    >>

--- client/src/controller/common/notification.controller.js
+++ client/src/controller/common/notification.controller.js
@@ -1,22 +1,50 @@
 import * as Notifications from "expo-notifications";
 
-export async function registerForPushNotifications() {
+export const registerForPushNotificationsAsync = async () => {
+    let token;
+
+    if (Platform.OS === "android") {
+        await Notifications.setNotificationChannelAsync(
+            "myNotificationChannel",
+            {
+                name: "A channel is needed for the permissions prompt to appear",
+                importance: Notifications.AndroidImportance.MAX,
+                vibrationPattern: [0, 250, 250, 250],
+                lightColor: "#FF231F7C",
+            },
+        );
+    }
 
     const { status: existingStatus } =
         await Notifications.getPermissionsAsync();
-
     let finalStatus = existingStatus;
-
     if (existingStatus !== "granted") {
         const { status } = await Notifications.requestPermissionsAsync();
         finalStatus = status;
     }
-
     if (finalStatus !== "granted") {
-        alert("Permission denied");
+        alert("Failed to get push token for push notification!");
         return;
     }
+    // Learn more about projectId:
+    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+    // EAS projectId is used here.
+    try {
+        const projectId =
+            Constants?.expoConfig?.extra?.eas?.projectId ??
+            Constants?.easConfig?.projectId;
+        if (!projectId) {
+            throw new Error("Project ID not found");
+        }
+        token = (
+            await Notifications.getExpoPushTokenAsync({
+                projectId,
+            })
+        ).data;
+        console.log(token);
+    } catch (e) {
+        token = `${e}`;
+    }
 
-    const token = (await Notifications.getExpoPushTokenAsync()).data;
     return token;
-}
+};
